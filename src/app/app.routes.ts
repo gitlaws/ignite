ee6777cbcbc6ef9c764a7cebb8464b1955c7b881
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-12 18:02:01 -0400

    Refactor routes and toolbar component
    - Update routes in app.routes.ts to include home path and remove commented out routes for projects and profile
    - Update toolbar.component.html to remove app-cube-logo and add a flame icon
    - Update toolbar.component.scss to add a flame glow effect and remove unused color variables in _colors.scss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/common/home/home.component').then(
+        (m) => m.HomeComponent
+      ),
+  },
+
+  {
+    path: 'Home',
+    loadComponent: () =>
+      import('./components/common/home/home.component').then(
+        (m) => m.HomeComponent
+      ),
+  },
+
+  //   {
+  //     path: 'projects',
+  //     loadComponent: () =>
+  //       import('./features/projects/projects.component').then(
+  //         (component) => component.ProjectsComponent
+  //       ),
+  //   },
+  //   {
+  //     path: 'profile',
+  //     loadComponent: () =>
+  //       import('./features/profile/profile.component').then(
+  //         (m) => m.ProfileComponent
+  //       ),
+  //   },
+  // other routes...
+];
