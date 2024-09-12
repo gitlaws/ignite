7c2c22134258ed13f5fb5f626f4a45b86f8c1299
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-12 17:14:59 -0400

    Refactor mobile menu component to add functionality and improve structure

--- src/app/components/common/mobile-menu/mobile-menu.component.ts
+++ src/app/components/common/mobile-menu/mobile-menu.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, OnDestroy, ElementRef } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-mobile-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './mobile-menu.component.html',
-  styleUrl: './mobile-menu.component.scss'
+  styleUrl: './mobile-menu.component.scss',
 })
 export class MobileMenuComponent {
+  // theme!: 'light' | 'dark';
+  isMenuOpen = false;
+  menuItems = [
+    { name: 'Projects', url: '/home' },
+    { name: 'Profile', url: '/about' },
+  ];
 
+  openLink(event: MouseEvent, url: string): void {
+    event.preventDefault(); // Prevent default anchor behavior
+    window.open(url, '_blank', 'noopener'); // Open link in a new tab
+  }
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit(): void {
+    // Add a click event listener to the document
+    document.addEventListener('click', this.documentClickHandler.bind(this));
+  }
+
+  ngOnDestroy(): void {
+    // Remove the click event listener from the document
+    document.removeEventListener('click', this.documentClickHandler.bind(this));
+  }
+
+  documentClickHandler(event: MouseEvent) {
+    // Check if the click event is coming from within the dropdown content
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      // If not, close the dropdown
+      this.isMenuOpen = false;
+    }
+  }
+
+  toggleMenu(event: MouseEvent) {
+    event.stopPropagation();
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
