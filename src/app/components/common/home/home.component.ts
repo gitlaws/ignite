c4ee10dcbdfdc690ce33cee6c08a8fb0ade32ef4
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-12 23:49:58 -0400

    Refactor home.component.html: Add shop-by-products component

--- src/app/components/common/home/home.component.ts
+++ src/app/components/common/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ShopByProductsComponent } from './shop-by-products/shop-by-products.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ShopByProductsComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
---
+++ src/app/components/common/home/shop-by-products/shop-by-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-by-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './shop-by-products.component.html',
+  styleUrl: './shop-by-products.component.scss'
+})
+export class ShopByProductsComponent {
+
+}
