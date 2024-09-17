1d2c6d9954a16c2e5c5217962391b29d698a94c2
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-17 18:04:15 -0400

    Refactor category-carousel component to implement image carousel functionality

--- src/app/components/common/home/category-carousel/category-carousel.component.ts
+++ src/app/components/common/home/category-carousel/category-carousel.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-category-carousel',
@@ -8,4 +8,71 @@ import { Component } from '@angular/core';
   templateUrl: './category-carousel.component.html',
   styleUrl: './category-carousel.component.scss',
 })
-export class CategoryCarouselComponent {}
+export class CategoryCarouselComponent implements OnInit {
+  images = ['image1.jpg', 'image2.jpg', 'image3.jpg'];
+  currentIndex = 0;
+
+  get transformStyle() {
+    return `translateX(-${this.currentIndex * 100}%)`;
+  }
+
+  ngOnInit() {
+    this.lazyLoadImages();
+  }
+
+  lazyLoadImages() {
+    const images = document.querySelectorAll('.carousel-image');
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const img = entry.target as HTMLImageElement;
+          img.src = img.dataset.src!;
+          observer.unobserve(img);
+        }
+      });
+    });
+
+    images.forEach((image) => observer.observe(image));
+  }
+
+  onImageLoad(index: number) {
+    if (
+      index === this.currentIndex &&
+      this.currentIndex < this.images.length - 1
+    ) {
+      const nextImage = document.querySelectorAll('.carousel-image')[
+        this.currentIndex + 1
+      ] as HTMLImageElement;
+      nextImage['src'] = nextImage.dataset.src!;
+    }
+  }
+
+  nextImage() {
+    if (this.currentIndex < this.images.length - 1) {
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0;
+    }
+  }
+
+  prevImage() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    } else {
+      this.currentIndex = this.images.length - 1;
+    }
+  }
+
+  goToImage(index: number) {
+    this.currentIndex = index;
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    if (event.key === 'ArrowRight') {
+      this.nextImage();
+    } else if (event.key === 'ArrowLeft') {
+      this.prevImage();
+    }
+  }
+}
