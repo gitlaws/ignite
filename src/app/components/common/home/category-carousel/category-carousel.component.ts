70b5c586b269195f980c0d07df4d6939f4bff204
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-17 17:24:15 -0400

    Refactor home component to include category carousel

---
+++ src/app/components/common/home/category-carousel/category-carousel.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-carousel',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './category-carousel.component.html',
+  styleUrl: './category-carousel.component.scss',
+})
+export class CategoryCarouselComponent {}
--- src/app/components/common/home/home.component.ts
+++ src/app/components/common/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ShopByCategoryComponent } from './shop-by-category/shop-by-category.component';
+import { CategoryCarouselComponent } from './category-carousel/category-carousel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ShopByCategoryComponent],
+  imports: [ShopByCategoryComponent, CategoryCarouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
