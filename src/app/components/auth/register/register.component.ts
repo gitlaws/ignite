6f164148f27d8a9f3bc550d949c2191c1eff8887
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-18 21:57:26 -0400

    Refactor home.component.html: Uncomment app-shop-by-category and app-category-carousel components

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registrationForm: FormGroup;
+  errorMessage: string;
 
+  constructor(private fb: FormBuilder, private afAuth: AngularFireAuth) {}
+
+  ngOnInit(): void {
+    this.registrationForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  async onSubmit() {
+    if (this.registrationForm.valid) {
+      const { name, email, password } = this.registrationForm.value;
+      try {
+        const userCredential = await this.afAuth.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+        await userCredential.user.updateProfile({ displayName: name });
+        // Handle successful registration (e.g., navigate to another page)
+      } catch (error) {
+        this.errorMessage = error.message;
+      }
+    }
+  }
 }
--- src/app/components/common/home/home.component.ts
+++ src/app/components/common/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { ShopByCategoryComponent } from './shop-by-category/shop-by-category.component';
 import { CategoryCarouselComponent } from './category-carousel/category-carousel.component';
+import { RegisterComponent } from '../../auth/register/register.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ShopByCategoryComponent, CategoryCarouselComponent],
+  imports: [
+    ShopByCategoryComponent,
+    CategoryCarouselComponent,
+    RegisterComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
