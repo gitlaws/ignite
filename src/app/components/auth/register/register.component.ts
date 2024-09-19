51b70bd3c711bb987d5a4ba37de719a720834cbe
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-18 22:12:42 -0400

    Refactor registration component HTML and TypeScript files: Remove error message display and unused code

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -11,41 +11,38 @@ import { ReactiveFormsModule } from '@angular/forms'; // Import ReactiveFormsMod
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit {
-  registrationForm!: FormGroup;
-  errorMessage: string = '';
-
-  constructor(private fb: FormBuilder, private afAuth: AngularFireAuth) {}
-
-  ngOnInit(): void {
-    this.registrationForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-    });
-  }
-
-  async onSubmit() {
-    if (this.registrationForm.valid) {
-      const { name, email, password } = this.registrationForm.value;
-      try {
-        const userCredential = await this.afAuth.createUserWithEmailAndPassword(
-          email,
-          password
-        );
-        if (userCredential.user) {
-          await userCredential.user.updateProfile({ displayName: name });
-          // Handle successful registration (e.g., navigate to another page)
-        } else {
-          this.errorMessage = 'User registration failed.';
-        }
-      } catch (error) {
-        if (error instanceof Error) {
-          this.errorMessage = error.message;
-        } else {
-          this.errorMessage = 'An unknown error occurred.';
-        }
-      }
-    }
-  }
+export class RegisterComponent {
+  // registrationForm!: FormGroup;
+  // errorMessage: string = '';
+  // constructor(private fb: FormBuilder, private afAuth: AngularFireAuth) {}
+  // ngOnInit(): void {
+  //   this.registrationForm = this.fb.group({
+  //     name: ['', Validators.required],
+  //     email: ['', [Validators.required, Validators.email]],
+  //     password: ['', [Validators.required, Validators.minLength(6)]],
+  //   });
+  // }
+  // async onSubmit() {
+  //   if (this.registrationForm.valid) {
+  //     const { name, email, password } = this.registrationForm.value;
+  //     try {
+  //       const userCredential = await this.afAuth.createUserWithEmailAndPassword(
+  //         email,
+  //         password
+  //       );
+  //       if (userCredential.user) {
+  //         await userCredential.user.updateProfile({ displayName: name });
+  //         // Handle successful registration (e.g., navigate to another page)
+  //       } else {
+  //         this.errorMessage = 'User registration failed.';
+  //       }
+  //     } catch (error) {
+  //       if (error instanceof Error) {
+  //         this.errorMessage = error.message;
+  //       } else {
+  //         this.errorMessage = 'An unknown error occurred.';
+  //       }
+  //     }
+  //   }
+  // }
 }
