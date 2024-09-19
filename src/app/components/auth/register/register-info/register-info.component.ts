3ee2303e8ffeeeba22258631657a0dd7e238ce9b
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-18 22:34:54 -0400

    Refactor registration component HTML and TypeScript files: Update registration form layout and styling

---
+++ src/app/components/auth/register/register-info/register-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-info.component.html',
+  styleUrl: './register-info.component.scss',
+})
+export class RegisterInfoComponent {}
--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,48 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms'; // Import ReactiveFormsModule
+import { ReactiveFormsModule } from '@angular/forms';
+import { RegisterInfoComponent } from './register-info/register-info.component'; // Import ReactiveFormsModule
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule], // Add ReactiveFormsModule here
+  imports: [CommonModule, ReactiveFormsModule, RegisterInfoComponent], // Add ReactiveFormsModule here
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {
-  // registrationForm!: FormGroup;
-  // errorMessage: string = '';
-  // constructor(private fb: FormBuilder, private afAuth: AngularFireAuth) {}
-  // ngOnInit(): void {
-  //   this.registrationForm = this.fb.group({
-  //     name: ['', Validators.required],
-  //     email: ['', [Validators.required, Validators.email]],
-  //     password: ['', [Validators.required, Validators.minLength(6)]],
-  //   });
-  // }
-  // async onSubmit() {
-  //   if (this.registrationForm.valid) {
-  //     const { name, email, password } = this.registrationForm.value;
-  //     try {
-  //       const userCredential = await this.afAuth.createUserWithEmailAndPassword(
-  //         email,
-  //         password
-  //       );
-  //       if (userCredential.user) {
-  //         await userCredential.user.updateProfile({ displayName: name });
-  //         // Handle successful registration (e.g., navigate to another page)
-  //       } else {
-  //         this.errorMessage = 'User registration failed.';
-  //       }
-  //     } catch (error) {
-  //       if (error instanceof Error) {
-  //         this.errorMessage = error.message;
-  //       } else {
-  //         this.errorMessage = 'An unknown error occurred.';
-  //       }
-  //     }
-  //   }
-  // }
+export class RegisterComponent implements OnInit {
+  registrationForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, router: Router) {}
+
+  ngOnInit() {
+    this.registrationForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      rememberMe: [false],
+    });
+  }
+
+  onSubmit() {
+    // Handle form submission (e.g., send data to server)
+    console.log(this.registrationForm.value);
+  }
 }
