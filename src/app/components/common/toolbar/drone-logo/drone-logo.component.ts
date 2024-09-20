0c75cd3fc60b37b4d910e2f2fb447b5bcf62b77a
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-19 20:53:40 -0400

    Refactor drone logo component: Update logo image and add router link

--- src/app/components/common/toolbar/drone-logo/drone-logo.component.ts
+++ src/app/components/common/toolbar/drone-logo/drone-logo.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-drone-logo',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './drone-logo.component.html',
-  styleUrl: './drone-logo.component.scss'
+  styleUrl: './drone-logo.component.scss',
 })
-export class DroneLogoComponent {
-
-}
+export class DroneLogoComponent {}
