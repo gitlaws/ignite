14834670823153bb3e06edc20303ba4a19492138
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-19 21:51:19 -0400

    Refactor toolbar component: Update styling and colors

--- src/app/components/common/toolbar/toolbar.component.ts
+++ src/app/components/common/toolbar/toolbar.component.ts
@@ -3,6 +3,7 @@ import { CubeLogoComponent } from './cube-logo/cube-logo.component';
 import { MobileMenuComponent } from '../mobile-menu/mobile-menu.component';
 import { IgniteLogoComponent } from './ignite-logo/ignite-logo.component';
 import { DroneLogoComponent } from './drone-logo/drone-logo.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
@@ -12,6 +13,7 @@ import { DroneLogoComponent } from './drone-logo/drone-logo.component';
     MobileMenuComponent,
     IgniteLogoComponent,
     DroneLogoComponent,
+    RouterLink,
   ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss',
