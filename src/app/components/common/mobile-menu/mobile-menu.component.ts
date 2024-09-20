137386de25c2fa6e571687b67625da4d090c0d7d
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-19 21:38:55 -0400

    Refactor mobile menu component: Update login and register links

--- src/app/components/common/mobile-menu/mobile-menu.component.ts
+++ src/app/components/common/mobile-menu/mobile-menu.component.ts
@@ -5,7 +5,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-mobile-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './mobile-menu.component.html',
   styleUrl: './mobile-menu.component.scss',
 })
@@ -13,8 +13,8 @@ export class MobileMenuComponent {
   // theme!: 'light' | 'dark';
   isMenuOpen = false;
   menuItems = [
-    { name: 'Projects', url: '/home' },
-    { name: 'Profile', url: '/about' },
+    { name: 'Login', url: '/login' },
+    { name: 'Register', url: '/register' },
   ];
 
   openLink(event: MouseEvent, url: string): void {
