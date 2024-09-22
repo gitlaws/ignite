7a0273d7528d7462cc6c6232cfbb26485efbe123
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-22 16:14:32 -0400

    Refactor routes: Remove unnecessary imports and update auth.service import

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,44 +1,4 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-// export const routes: Routes = [
-//   {
-//     path: '',
-//     loadComponent: () =>
-//       import('./components/common/home/home.component').then(
-//         (m) => m.HomeComponent
-//       ),
-//   },
-//   {
-//     path: 'home',
-//     loadComponent: () =>
-//       import('./components/common/home/home.component').then(
-//         (m) => m.HomeComponent
-//       ),
-//   },
-//   {
-//     path: 'register',
-//     loadComponent: () =>
-//       import('./components/auth/register/register.component').then(
-//         (m) => m.RegisterComponent
-//       ),
-//   },
-//   {
-//     path: 'login',
-//     loadComponent: () =>
-//       import('./components/auth/login/login.component').then(
-//         (m) => m.LoginComponent
-//       ),
-//   },
-//   {
-//     path: 'signout',
-//     loadComponent: () =>
-//       import('./components/auth/signout/signout.component').then(
-//         (m) => m.SignoutComponent
-//       ),
-//   },
-//   { path: '', redirectTo: '/login', pathMatch: 'full' },
-// ];
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
@@ -71,9 +31,3 @@ export const routes: Routes = [
   },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'; // Import FormsModule
-import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { RegisterInfoComponent } from './register-info/register-info.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, FormsModule, RegisterInfoComponent], // Add FormsModule to imports array
+  imports: [CommonModule, FormsModule, RegisterInfoComponent, RouterLink], // Add FormsModule to imports array
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
