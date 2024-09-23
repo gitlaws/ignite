7dc202809d8f5a5b6d733ee18ad828d8cd1842bb
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-23 18:35:43 -0400

    Refactor login component: Add login-info component

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -3,11 +3,12 @@ import { AuthService } from '../../../services/auth.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { LoginInfoComponent } from './login-info/login-info.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink],
+  imports: [CommonModule, FormsModule, RouterLink, LoginInfoComponent],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
