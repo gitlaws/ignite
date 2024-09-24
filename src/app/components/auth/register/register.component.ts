f8900554444c4ecb42ae02197e9fc1771c389902
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-23 21:51:39 -0400

    Refactor: Add snackbar service to register component for displaying registration success message

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,28 +1,33 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../../../services/auth.service';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { RegisterInfoComponent } from './register-info/register-info.component';
+import { AuthService } from '../../../services/auth.service';
+import { SnackbarService } from '../../../services/snackbar.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, FormsModule, RegisterInfoComponent, RouterLink], // Add FormsModule to imports array
+  imports: [CommonModule, FormsModule, RegisterInfoComponent, RouterLink],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
+  providers: [SnackbarService], // Provide the SnackbarService here
 })
 export class RegisterComponent {
   password!: string;
   email!: string;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private snackbarService: SnackbarService
+  ) {}
 
   register() {
     this.authService
       .register(this.email, this.password)
       .then(() => {
-        alert(
+        this.snackbarService.show(
           'Registration successful! Please check your email to verify your account.'
         );
       })
