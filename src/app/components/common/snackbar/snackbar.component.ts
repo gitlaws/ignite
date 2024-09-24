6ff8390ad093904b929e5e07231420aa9fd25239
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-23 21:43:44 -0400

    Refactor: Add snackbar component styles and functionality

--- src/app/components/common/snackbar/snackbar.component.ts
+++ src/app/components/common/snackbar/snackbar.component.ts
@@ -1,10 +1,38 @@
+// src/app/components/snackbar/snackbar.component.ts
 import { Component, Input } from '@angular/core';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+import { SnackbarService } from '../../../services/snackbar.service';
 
 @Component({
   selector: 'app-snackbar',
   standalone: true,
-  imports: [],
   templateUrl: './snackbar.component.html',
-  styleUrl: './snackbar.component.scss',
+  styleUrls: ['./snackbar.component.scss'],
 })
-export class SnackbarComponent {}
+export class SnackbarComponent {
+  @Input() public message = '';
+  public isShown = false;
+
+  constructor(public snackbarService: SnackbarService) {}
+
+  public ngOnInit(): void {
+    this.snackbarService.snackbar$.subscribe((value) => {
+      this.showMessage(value);
+    });
+  }
+
+  public showMessage(message: string): void {
+    this.isShown = true;
+    this.message = message;
+
+    const subscription = of(null)
+      .pipe(delay(2900))
+      .subscribe({
+        complete: () => {
+          this.isShown = false;
+          subscription.unsubscribe();
+        },
+      });
+  }
+}
