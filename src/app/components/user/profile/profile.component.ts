5d175ecdde06383b675b3e786e34904265afea08
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-25 13:14:08 -0400

    Refactor: Remove unused profile component code

--- src/app/components/user/profile/profile.component.ts
+++ src/app/components/user/profile/profile.component.ts
@@ -1,12 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { User } from 'firebase/auth';
+import { User } from 'firebase/auth'; // Correct import for User type
 import { AuthService } from '../../../services/auth.service';
 import { RouterLink, RouterModule, Router } from '@angular/router';
-import { finalize, map } from 'rxjs/operators'; // Import 'map' from 'rxjs/operators'
-import { AngularFireStorage } from '@angular/fire/compat/storage';
-import { Observable, of } from 'rxjs'; // Import 'of' from 'rxjs'
 
 @Component({
   selector: 'app-profile',
@@ -19,14 +16,8 @@ export class ProfileComponent implements OnInit {
   user: User | null = null;
   displayName!: string;
   photoURL!: string;
-  uploadPercent: Observable<number> = of(0); // Initialize with an empty observable
-  downloadURL: Observable<string> = of(''); // Initialize with an empty observable
 
-  constructor(
-    private authService: AuthService,
-    private storage: AngularFireStorage,
-    private router: Router
-  ) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
     this.authService.getUser().subscribe((user) => {
@@ -37,26 +28,6 @@ export class ProfileComponent implements OnInit {
         alert('Please verify your email to access your profile.');
       }
     });
-  }
-
-  uploadFile(event: any) {
-    const file = event.target.files[0];
-    const uid = this.authService.getCurrentUserUid();
-    if (uid) {
-      const filePath = `profile_pictures/${uid}`;
-      const fileRef = this.storage.ref(filePath);
-      const task = this.storage.upload(filePath, file);
-
-      this.uploadPercent = task.percentageChanges().pipe(
-        map((percentage) => percentage ?? 0) // Transform 'undefined' to '0'
-      );
-      task
-        .snapshotChanges()
-        .pipe(finalize(() => (this.downloadURL = fileRef.getDownloadURL())))
-        .subscribe();
-    } else {
-      console.error('No user is currently logged in');
-    }
   }
 
   updateProfile() {
