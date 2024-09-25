3ae724961b22b30497f3331f0617c289e12b1527
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-24 21:33:48 -0400

    Refactor: Update home component HTML to use routerLink instead of onclick

--- src/app/components/auth/login/login-info/login-info.component.ts
+++ src/app/components/auth/login/login-info/login-info.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login-info',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterModule],
   templateUrl: './login-info.component.html',
-  styleUrl: './login-info.component.scss'
+  styleUrl: './login-info.component.scss',
 })
-export class LoginInfoComponent {
-
-}
+export class LoginInfoComponent {}
