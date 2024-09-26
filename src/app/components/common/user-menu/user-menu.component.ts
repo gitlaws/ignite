d2bbbc2df9186c147bed9d4d0302e219c2ac4f41
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-26 18:39:15 -0400

    init

--- src/app/components/common/user-menu/user-menu.component.ts
+++ src/app/components/common/user-menu/user-menu.component.ts
@@ -1,27 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-menu.component.html',
   styleUrls: ['./user-menu.component.scss'],
 })
 export class UserMenuComponent implements OnInit {
-  user: any = {};
+  isLoggedIn: boolean = false; // Add this property
+  user: any = {}; // Assuming you have a user object
 
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.authService.getUser().subscribe((user) => {
       this.user = user;
+      this.isLoggedIn = !!user; // Set isLoggedIn based on user presence
     });
   }
 
-  logout() {
+  logout(): void {
     this.authService.logout().then(() => {
       this.user = {};
+      this.isLoggedIn = false; // Update isLoggedIn on logout
     });
   }
 }
