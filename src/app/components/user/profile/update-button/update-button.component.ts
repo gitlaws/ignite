d248af4c1117b2e78afdf561a3706419ebd3b773
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2024-09-26 10:01:53 -0400

    update

---
+++ src/app/components/user/profile/update-button/update-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-button.component.html',
+  styleUrl: './update-button.component.scss'
+})
+export class UpdateButtonComponent {
+
+}
