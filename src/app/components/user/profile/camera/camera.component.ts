2d4a3fb0c21d5f34ab4028dcbdf990b150385b5a
Staś <lawmski@gmail.com>
2024-11-04 21:14:45 -0500

    updated camera

--- src/app/components/user/profile/camera/camera.component.ts
+++ src/app/components/user/profile/camera/camera.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-camera',
   standalone: true,
   imports: [],
   templateUrl: './camera.component.html',
-  styleUrl: './camera.component.scss'
+  styleUrls: ['./camera.component.scss'],
 })
 export class CameraComponent {
+  capturedImage: string | undefined;
+  tempPhotoURL: string | undefined;
+  user: { photoURL: string | undefined };
+  disabled: boolean = false;
 
+  constructor() {
+    this.user = { photoURL: undefined };
+  }
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+    });
+
+    this.capturedImage = image.webPath;
+  }
 }
